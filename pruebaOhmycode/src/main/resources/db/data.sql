2cd44c74c899ce8b777e08d11eb513337db9ebc7
OscarPM24 <oscarpeme2013@gmail.com>
2025-07-27 19:36:45 +0200

    arregladas contraseñas de ejemplo en data.sql

--- pruebaOhmycode/src/main/resources/db/data.sql
+++ pruebaOhmycode/src/main/resources/db/data.sql
@@ -1,7 +1,7 @@
 INSERT IGNORE INTO user (id, name, username, password, street, city, zipcode, country) VALUES
-    (1, 'Oscar', 'oscar434', '$2a$10$rXK1g7QH8VJ5hUaLJrn8KeWE5ydFt5WKj2mQJ8N9jJHK7L9mN0oP6', 'Carrer de Rosselló', 'Barcelona', '08001', 'España'),
-    (2, 'Javier', 'javi123', '$2a$10$mN5hK8jL7Q3vB9cD2eF6gH4iJ1kL8mN9oP0qR2sT4uV6wX8yZ0aB1', 'Carrer de Pallars', 'Paris', '24684', 'Francia'),
-    (3, 'Ivan', 'ivan785', '$2a$10$sT8uV6wX0yZ2aB4cD6eF8gH1iJ3kL5mN7oP9qR1sT3uV5wX7yZ9aB', 'Carrer de Roger de Flor', 'New York', '35796', 'EEUU');
+    (1, 'Oscar', 'oscar434', '$2a$12$/0LLVvaEvP3TDU7XvsjpWOPuohFbU3kR7jVC.a6ZEIqoSx4vSR9ti', 'Carrer de Rosselló', 'Barcelona', '08001', 'España'),
+    (2, 'Javier', 'javi123', '$2a$12$VhU5wD/OrP0Iy1u2zuR7tudwz06oteA3njjUcnEbh/bTDK.RMZQse', 'Carrer de Pallars', 'Paris', '24684', 'Francia'),
+    (3, 'Ivan', 'ivan785', '$2a$12$DaJcHewx0lAZcIEAbZKZw.rBasrVS4Wmvrtvvm3n46z/wcvXr94G2', 'Carrer de Roger de Flor', 'New York', '35796', 'EEUU');
 
 /* Passwords de ejemplo:
     - Oscar: 1234
